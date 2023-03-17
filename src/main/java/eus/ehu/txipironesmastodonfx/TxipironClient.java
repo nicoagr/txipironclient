08b810db1bb1430c8a993c4f06c323428a234425
nicoagr <github@nico.eus>
2023-03-17 12:20:41 +0100

    Add icon(s)

--- src/main/java/eus/ehu/txipironesmastodonfx/TxipironClient.java
+++ src/main/java/eus/ehu/txipironesmastodonfx/TxipironClient.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -40,6 +41,7 @@ public class TxipironClient extends Application {
 
         scene = new Scene(authWindow.ui);
         stage.setTitle("Txipiron Client [v1.0] - a Mastodon Client - Account Management");
+        stage.getIcons().add(new Image("file:src/main/resources/eus/ehu/txipironesmastodonfx/dark_filled_1000.jpg"));
         stage.setScene(scene);
         stage.show();
     }
