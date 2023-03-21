988726835a1b7546675c3ba6c92e30c3911cb0a2
marcosChoucino <Marcos.choucino@gmail.com>
2023-03-21 19:29:52 +0100

    TootCellController  for Sprint 1 done and it fxml actualized :p

--- src/main/java/eus/ehu/txipironesmastodonfx/controllers/windowControllers/TootCellController.java
+++ src/main/java/eus/ehu/txipironesmastodonfx/controllers/windowControllers/TootCellController.java
@@ -2,10 +2,19 @@ package eus.ehu.txipironesmastodonfx.controllers.windowControllers;
 
 
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import eus.ehu.txipironesmastodonfx.controllers.main.MainWindowController;
+import eus.ehu.txipironesmastodonfx.domain.Account;
+import eus.ehu.txipironesmastodonfx.domain.Toot;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 public class TootCellController   {
 
@@ -22,6 +31,8 @@ public class TootCellController   {
     private Label date;
 
     @FXML
+    private AnchorPane anchor;
+    @FXML
     private Label numComments;
 
     @FXML
@@ -36,16 +47,42 @@ public class TootCellController   {
     @FXML
     private Label username;
 
+
     @FXML
-    void initialize() {
-        assert Id != null : "fx:id=\"Id\" was not injected: check your FXML file 'tootcell.fxml'.";
-        assert date != null : "fx:id=\"date\" was not injected: check your FXML file 'tootcell.fxml'.";
-        assert numComments != null : "fx:id=\"numComments\" was not injected: check your FXML file 'tootcell.fxml'.";
-        assert numLikes != null : "fx:id=\"numLikes\" was not injected: check your FXML file 'tootcell.fxml'.";
-        assert numReboots != null : "fx:id=\"numReboots\" was not injected: check your FXML file 'tootcell.fxml'.";
-        assert tootinfo != null : "fx:id=\"tootinfo\" was not injected: check your FXML file 'tootcell.fxml'.";
-        assert username != null : "fx:id=\"username\" was not injected: check your FXML file 'tootcell.fxml'.";
+    private ImageView imagen;
+    private MainWindowController master;
+
+
+    public TootCellController(Toot toot) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("eus/ehu/txipironesmastodonfx/tootcell.fxml"));
+        fxmlLoader.setController(this);
+        try {
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // set the values for the account cell
+            this.Id.setText(toot.account.id);
+            this.username.setText(toot.account.acct);
+            this.imagen.setImage(new Image(toot.account.avatar));
+            this.date.setText((toot.created_at));
+            this.tootinfo.setText(toot.content);
+            this.numLikes.setText(Integer.toString(toot.favourites_count));
+            this.numReboots.setText(Integer.toString(toot.reblogs_count));
+            this.numComments.setText(Integer.toString(toot.replies_count));
+
+
+
 
     }
 
+    public AnchorPane getUI() {
+        return anchor;
+    }
+    public void setReference(MainWindowController thisclass) {
+        this.master = thisclass;
+    }
+    
+    
 }
