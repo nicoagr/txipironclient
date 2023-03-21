407e49f3ad48a3f79e7e7855578c542fc425bdec
marcosChoucino <Marcos.choucino@gmail.com>
2023-03-21 18:37:37 +0100

    followcell controler for Sprint 1 done and it fxml actualized :p

--- src/main/java/eus/ehu/txipironesmastodonfx/controllers/windowControllers/followCellController.java
+++ src/main/java/eus/ehu/txipironesmastodonfx/controllers/windowControllers/followCellController.java
@@ -1,15 +1,27 @@
 package eus.ehu.txipironesmastodonfx.controllers.windowControllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import eus.ehu.txipironesmastodonfx.controllers.auth.AuthWindowController;
+import eus.ehu.txipironesmastodonfx.controllers.main.MainWindowController;
+import eus.ehu.txipironesmastodonfx.domain.Account;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 public class followCellController {
-
+    private MainWindowController master;
+    @FXML
+    private Button followbutton;
+    @FXML
+    private AnchorPane anchor;
     @FXML
     private ResourceBundle resources;
 
@@ -19,8 +31,7 @@ public class followCellController {
     @FXML
     private Label Id;
 
-    @FXML
-    private Button followbutton;
+
 
     @FXML
     private ImageView icon;
@@ -31,19 +42,53 @@ public class followCellController {
     @FXML
     private Label username;
 
-    @FXML
-    void followAction(ActionEvent event) {
 
+
+    /**
+     * Constructor for the controller.
+     * It will load itself at take consciousness (set itself as controller)
+     * Also it will set the corresponding values for the account cell.
+     *
+     * @param account (Account) - The account to be displayed
+     */
+    public followCellController(Account account) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("eus/ehu/txipironesmastodonfx/followcell.fxml"));//ejemplo: /eus/ehu/txipironesmastodonfx/auth/authaccocell.fxml
+        fxmlLoader.setController(this);
+        try {
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // set the values for the account cell
+        Id.setText(account.id);
+        username.setText(account.acct);
+        info.setText(account.note);
+        icon.setImage(new Image(account.avatar));
     }
 
-    @FXML
-    void initialize() {
-        assert Id != null : "fx:id=\"Id\" was not injected: check your FXML file 'followcell.fxml'.";
-        assert followbutton != null : "fx:id=\"followbutton\" was not injected: check your FXML file 'followcell.fxml'.";
-        assert icon != null : "fx:id=\"icon\" was not injected: check your FXML file 'followcell.fxml'.";
-        assert info != null : "fx:id=\"info\" was not injected: check your FXML file 'followcell.fxml'.";
-        assert username != null : "fx:id=\"username\" was not injected: check your FXML file 'followcell.fxml'.";
-
+    /**
+     * Getter for the UI (AnchorPane)
+     * This method will be used by the AuthWindowController
+     * in order to display custom cells in the listview
+     *
+     * @return (AnchorPane) - The UI of the controller
+     */
+    public AnchorPane getUI() {
+        return anchor;
     }
+
+    /**
+     * Setter for the reference to the auth window controller
+     *
+     * @param thisclass (AuthWindowController) - The reference to the auth window controller
+     */
+    public void setReference(MainWindowController thisclass) {
+        this.master = thisclass;
+    }
+
+
+
+
 
 }
