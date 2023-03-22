206ec7e7542b958ddf20b150d701e6e178bb1b18
marcosChoucino <Marcos.choucino@gmail.com>
2023-03-22 12:45:08 +0100

    avanced on the controllers

---
+++ src/main/java/eus/ehu/txipironesmastodonfx/controllers/windowControllers/HeaderCellController.java
@@ -0,0 +1,39 @@
+package eus.ehu.txipironesmastodonfx.controllers.windowControllers;
+
+import eus.ehu.txipironesmastodonfx.domain.Account;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+public class HeaderCellController {
+
+    @FXML
+    private Label info;
+    @FXML
+    private AnchorPane anchor;
+
+    public HeaderCellController(Account account) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/eus/ehu/txipironesmastodonfx/auth/headercell.fxml"));
+        fxmlLoader.setController(this);
+        try {
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+       // Label.
+
+              //  (account.acct);
+
+    }
+
+    public AnchorPane getUI() {
+        return anchor;
+    }
+
+}
